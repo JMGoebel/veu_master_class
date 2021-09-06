2fcd3c5965c2ac1030682f68e74324951bd39679
Jason M. Goebel <goebel.jason@gmail.com>
2021-09-05 21:48:33 -0500

    ADDS Dynamic Routes For Thread
    * Setup PageThreadShow
    * Add route for pageThreadShow

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,16 @@
 import PageHome from "@/pages/PageHome";
+import PageThreadShow from "@/pages/PageThreadShow";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: PageHome,
+  },
+  {
+    path: "/thread/:id",
+    name: "ThreadShow",
+    component: PageThreadShow,
   },
 ];
 
