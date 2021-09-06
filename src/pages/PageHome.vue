1f6b58f03f8722f5b76406ea68f8d0272d83373c
Jason M. Goebel <goebel.jason@gmail.com>
2021-09-06 00:00:37 -0500

    Tread List * Clean Up PageHome * ADDS ThreadList ** Style ThreadList ** Set up routes

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,38 +1,20 @@
 <template>
   <div>Hello</div>
-  <div v-for="thread in threads" :key="thread.id">
-    <h2>{{ thread.title }}</h2>
-    <div v-for="postId in thread.posts" :key="postId">
-      <app-comment
-        :post="getPostById(postId)"
-        :user="getUserById(getPostById(postId).userId)"
-      />
-    </div>
-  </div>
+  <thread-list :threads="threads" />
 </template>
 
 <script>
 import db from "@/data.json";
-import AppComment from "@/components/AppComment";
+import ThreadList from "../components/ThreadList.vue";
 
 export default {
   components: {
-    AppComment,
+    ThreadList,
   },
   data() {
     return {
       threads: db.threads,
-      posts: db.posts,
-      users: db.users,
     };
-  },
-  methods: {
-    getPostById(id) {
-      return this.posts.find((p) => p.id === id);
-    },
-    getUserById(id) {
-      return this.users.find((u) => u.id === id);
-    },
   },
 };
 </script>
